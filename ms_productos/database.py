4855ae8a2e2096b3fe24598e1487226a59bef1ee
Mija9000 <mijail.saltsin@utec.edu.pe>
2025-09-27 12:17:11 -0500

    FINAL

--- ms_productos/database.py
+++ ms_productos/database.py
@@ -1,7 +1,7 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://db:27017")  # "db" = servicio Mongo en docker-compose
 DB_NAME = os.getenv("DB_NAME", "ecommerce_db")
 
 client = AsyncIOMotorClient(MONGO_URL)
