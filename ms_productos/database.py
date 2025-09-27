30a944fc515eb7718737ce1eeec9d800e0db03d6
Mija9000 <mijail.saltsin@utec.edu.pe>
2025-09-27 10:55:53 -0500

    Actualización de Dockerfile y docker-compose.yml para MV

--- ms_productos/database.py
+++ ms_productos/database.py
@@ -1,7 +1,8 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL = "mongodb://localhost:27017"  # ⚠️ cámbialo si usas Atlas
-DB_NAME = "ecommerce_db"
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+DB_NAME = os.getenv("DB_NAME", "ecommerce_db")
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client[DB_NAME]
